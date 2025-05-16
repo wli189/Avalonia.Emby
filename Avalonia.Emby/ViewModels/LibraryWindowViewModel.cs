e46e9a744e99dfd10e1e630a0021c5c398341c5f
Weitao Li <weitaoli189@gmail.com>
2025-05-15 20:23:14 -0500

    Library window connect to main window

---
+++ Avalonia.Emby/ViewModels/LibraryWindowViewModel.cs
@@ -0,0 +1,13 @@
+﻿using Avalonia.Emby.Models;
+
+namespace Avalonia.Emby.ViewModels;
+
+public class LibraryWindowViewModel
+{
+    private readonly Account _account;
+
+    public LibraryWindowViewModel(Account account)
+    {
+        _account = account;
+    }
+}
---
+++ Avalonia.Emby/Views/LibraryWindow.axaml.cs
@@ -0,0 +1,13 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Avalonia.Emby.Views;
+
+public partial class LibraryWindow : Window
+{
+    public LibraryWindow()
+    {
+        InitializeComponent();
+    }
+}
